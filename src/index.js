4eed88326dc3602efa3ac96fc30528515d662b8f
vogelfrei <mr.boot.sasha@gmail.com>
2021-02-24 17:07:18 +0300

    task solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length == 0) return 0;
+  let minV = array[0];
+  array.filter((item) => {
+    return item < minV ? minV = item : 0;
+  }
+  );
+  return minV;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length == 0) return 0;
+  let maxV = array[0];
+  array.filter((item) => {
+    return item > maxV ? maxV = item : 0;
+  }
+  );
+  return maxV;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length == 0) return 0;
+  let sum = 0;
+  array.map(item => sum += item);
+  return array.length > 0 ? sum/array.length : 0;
 }
